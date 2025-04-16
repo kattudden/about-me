0df04f24f31dbc3ddac68d5b6e9cb7509022722b
Michael Lehmann <lehmschi@gmail.com>
2025-04-16 17:07:31 +0200

    Update devenv.

--- devenv.nix
+++ devenv.nix
@@ -4,8 +4,12 @@
   config,
   ...
 }: {
-  # https://devenv.sh/languages/
+  # https://devenv.sh/languages/go/
   languages.go.enable = true;
+
+  # https://devenv.sh/shell/
+  packages = [pkgs.zsh];
+  shell = pkgs.zsh;
 
   # See full reference at https://devenv.sh/reference/options/
 }
